175155e046ac96ba8fd01aa572ee62115c1eec65
golurgz17 <95138056+golurgz17@users.noreply.github.com>
2021-11-27 21:06:51 +0530

    Add files via upload

---
+++ if else.c
@@ -0,0 +1,21 @@
+/*A quick demo of nested if-else */
+#include <stdio.h>
+int main()
+  {     
+       int i;
+       
+       printf(Enter either 1 or2");
+       scanf("%d",&i);
+       
+       if(i==1)
+       printf("You would go to heaven !\n");
+       else
+       {
+           
+           if(i==2)
+           printf("Hell was created with you in mind\n");
+           else
+           printf("How about mother earth !\n");
+       }
+       return 0;
+       }
